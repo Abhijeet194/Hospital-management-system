a21a5c9b4b45942bf6b957d3212506213bff69f1
Abhijeet194 <abhijeetkumar20059955@gmail.com>
2026-04-24 19:06:09 +0530

    fixes bugs

--- app.py
+++ app.py
@@ -530,4 +530,4 @@ def logout():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 10000)), debug=True)
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 10000)))
